fd7bbc1ce98e3d8ddb7254e00f9a700ed21c2726
roger <igor.onyshchenko@gmail.com>
2012-05-10 17:32:59 +0300

    Test

--- dal/pic.py
+++ dal/pic.py
@@ -15,5 +15,5 @@ class pic(base.base):
         return result[0][0]
         
     def getLast(sefl, count):
-        self.cur.execute("select limit %s id_pic from pic order by id_pc desc;")
+        self.cur.execute("select limit %s id_pic from pic order by id_pc desc;", (count,))
         return self.cur.fetchall()
