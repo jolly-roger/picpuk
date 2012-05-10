e3644f77118803b9592b1644d8b239ed92108702
roger <igor.onyshchenko@gmail.com>
2012-05-10 17:11:06 +0300

    Test

--- sql/addpic.sql
+++ sql/addpic.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE FUNCTION addpic(userid numeric)
+CREATE OR REPLACE FUNCTION addpic(outeruserid varchar(128))
  RETURNS boolean
 AS $BODY$
 declare
     local_id_pic numeric;
 BEGIN
-    insert into pic (user_id) values (userid) returning id_pic into local_id_pic;
+    insert into pic (user_id) values ((select * from getuserid(outeruserid))) returning id_pic into local_id_pic;
 
     return local_id_pic;
 END;
