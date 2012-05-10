4e1cc52a3ac0c9344c0d8b6c0eac76381a85cdb2
roger <igor.onyshchenko@gmail.com>
2012-05-10 14:36:21 +0300

    Test

--- picpuk.py
+++ picpuk.py
@@ -32,7 +32,7 @@ class picpuk(object):
     @cherrypy.expose
     @authorization.isAuthorized
     def upload(self, fileContent):
-        cherrypy.log.error(fileContent)
+        cherrypy.log.error(str(fileContent))
 
 
 def error_page_default(status, message, traceback, version):
