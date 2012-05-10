8971164fac3a41fb43d88f2601f7b688ebb11791
roger <igor.onyshchenko@gmail.com>
2012-05-10 18:13:51 +0300

    T

--- sql/pic.sql
+++ sql/pic.sql
@@ -2,8 +2,8 @@ create sequence id_pic_seq minvalue 0 start 0;
 
 
 create table pic (
-	id_pic numeric primary key not null default nextval('id_pic_seq'),	
-	user_id numeric
+	id_pic bigint primary key not null default nextval('id_pic_seq'),	
+	user_id bigint
 );
 
 create index "pic_user_id_idx" on pic (user_id);
