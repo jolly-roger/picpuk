b04028b40a4b7ad98270d40258a6a86e7c8cdffa
roger <igor.onyshchenko@gmail.com>
2012-05-21 16:19:36 +0300

    i

---
+++ authconf.py
@@ -0,0 +1,10 @@
+APP_ID = "376067512445754"
+APP_SECRET = "67b9f5a090d71082ea7da48afd408ab3"
+
+FACEBOOK_ACCESS_TOKEN = 'facebook_access_token'
+GOOGLE_ACCESS_TOKEN = 'google_access_token'
+
+
+AUTHORIZE_CALLBACK_URL = "http://picpuk.ru/authorizecallback"
+AUTHENTICATE_CALLBACK_URL = "http://picpuk.ru/authenticatecallback"
+IS_SIGNED_REQUEST = "signed_request"
