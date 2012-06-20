bca1c87e6d222c3d5a033455d99d80f83072b4ee
roger <igor.onyshchenko@gmail.com>
2012-06-20 15:30:39 +0300

    i

--- largefileupload.py
+++ largefileupload.py
@@ -10,7 +10,7 @@ class largeFileUpload(object):
             <h2>Upload a file</h2>
             <form action="upload" method="post" enctype="multipart/form-data">
             filename: <input type="file" name="sourceData" /><br />
-            <input type="submit" />
+            <input type="submit" name="doUpload" value="Upload" />
             </form>
             <h2>Download a file</h2>
             <a href='download'>This one</a>
